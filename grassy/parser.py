be83cbdde432ba3044905f1c1d8eb24e543e7e12
venkat24 <venkatramansrikanth@outlook.com>
2017-03-16 00:45:46 +0530

    Jinja Templating

--- grassy/parser.py
+++ grassy/parser.py
@@ -1,10 +1,13 @@
 import markdown
 import sys, os
+import re
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 class BlogProcessor:
     def __init__(self):
         self.blogList = []
         self.populateBlogs()
+        self.renderTemplates()
 
     def populateBlogs(self):
         root_dir = "blogs"
@@ -14,7 +17,19 @@ class BlogProcessor:
                 with open(os.path.join(blog_dir,file),'r') as md_file:
                     markdown_text = md_file.read()
                     html_text = markdown.markdown(markdown_text)
-                    print(html_text)
+                    self.blogList.append({
+                        "title"   : file,
+                        "content" : html_text
+                    })
+
+    def renderTemplates(self):
+        template_dir = "templates"
+        template_full_dir = os.path.abspath(template_dir)
+        env = Environment(
+            loader=PackageLoader('grassy', os.path.join('..','templates'))
+        )
+        template = env.get_template('main.html')
+        print(template.render(posts=self.blogList))
 
 if __name__=="__main__":
     blog = BlogProcessor()
